0579d42044978eb25288236f66d0495c3d6c4f95
Sean Tuan <b03611001@ntu.edu.tw>
2018-12-21 15:15:28 +0800

    update

--- src/evaluate.py
+++ src/evaluate.py
@@ -8,7 +8,7 @@ import cv2
 import numpy as np
 from argparse import ArgumentParser
 from utils.shell import Shell, Point
-from utils.helper import measureFuso
+from utils.helper import measureFuso, rescaleData
 from utils.mapping import mapping
 
 
@@ -27,10 +27,14 @@ if __name__ == '__main__':
 	videos.sort()
 	annotations.sort()
 	testdata = list(zip(videos, annotations))
+	errors = list()
+	total_shells = 0
+	detected_shells = 0
 
 	for data in testdata:
 		video_filename, annotation_filename = data
 
+		# detection
 		cap = cv2.VideoCapture(video_filename)
 		points = list()
 		_, snapshot = cap.read()
@@ -38,10 +42,44 @@ if __name__ == '__main__':
 		mapping(cap, points)
 		cap.release()
 
+		rescaleData(points, fuso_length)
+		total_shells += len(points)		
+
+		# annotation
 		true_points = list()
 		with open(annotation_filename, newline='') as csvfile:
 			reader = csv.DictReader(csvfile)
 			for row in reader:
 				true_points.append(Point([row['x'], row['y'], 0, 0]))
 
+		rescaleData(true_points, fuso_length)
+		detected_shells += len(true_points)
+
+		# Hungarian algorithm
+		dim = max([len(true_points), len(points)])
+		costs = np.zeros((dim, dim))
+		for i, p in enumerate(true_points):
+			for j, tp in enumerate(points):
+				costs[i][j] = np.linalg.norm(np.array([tp.x, tp.y]) - np.array([p.x, p.y]))
+			if len(points) < dim:
+				for j in range(len(points), dim):
+					costs[i][j] = max(costs[i]) # dummy
+
+		row_idx, col_idx = linear_sum_assignment(costs)
+		for i in row_idx:
+			if col_idx[i] > len(points) - 1:
+				# true points > detected points
+				pass
+
+			elif row_idx[i] > len(true_points) - 1:
+				# true points < detected points (usually this won't happen)
+				pass
+
+			else:
+				# normal condition
+				errors.append(costs[row_idx[i]][col_idx[i]])
+
+	print("Total shells in the test data: {}".format(total_shells))
+	print("Total shells detected by the algorithm: {}".format(detected_shells))
+	print("Mean error: {}".format(np.mean(np.array(errors))))
 
